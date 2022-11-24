36515d5fb181e0ad5ab0e814c02f3e22b97b76c9
Rajanikanth <rajanikanthraju.r@gmail.com>
2022-11-24 17:48:08 +0530

    added depends on and created 16 subnets to accomadate 500 hosts in each network

--- VPC/inputs.tf
+++ VPC/inputs.tf
@@ -1,11 +1,23 @@
 variable "infra_region" {
-    type = string
-    default = "ap-southeast-1"
-    description = "This is the default region where infra will be created"
+  type        = string
+  default     = "ap-southeast-1"
+  description = "This is the default region where infra will be created"
 }
 variable "vpc_cidr" {
-    type=string
-    default = "192.168.0.0/16"
-    description = "This is default CIDR"
-  
+  type        = string
+  default     = "192.168.0.0/16"
+  description = "This is default CIDR"
+
+}
+variable "no_of_subnets" {
+  type        = number
+  default     = 16
+  description = "This is to set count no of required resources"
+
+}
+variable "subnet_tag_name" {
+  type        = string
+  default     = "subnetfromtf"
+  description = "This is the value to set the subnet name"
+
 }
--- VPC/network.tf
+++ VPC/network.tf
@@ -1,6 +1,25 @@
+
+#Creating VPC in Singapore region
 resource "aws_vpc" "vnet" {
-    cidr_block = var.vpc_cidr
-    tags = {
-      "Name" = "vpcfromtf"
-    }
+  cidr_block = var.vpc_cidr
+  tags = {
+    "Name" = "vpcfromtf"
+  }
 }
+
+#create 16 subnets each should accompany 500 hosts ipv4 addresses
+resource "aws_subnet" "subnet" {
+  count             = var.no_of_subnets
+  vpc_id            = aws_vpc.vnet.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 7, count.index)
+  availability_zone = format("${var.infra_region}%s", count.index % 2 == 0 ? "a" : "b")
+  tags = {
+    "Name" = format("${var.subnet_tag_name}%d", count.index + 1)
+  }
+
+  depends_on = [
+    aws_vpc.vnet
+  ]
+
+}
+
