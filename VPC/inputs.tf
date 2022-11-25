281816ff37cb602ea0003bec21b647a22a06fed9
Rajanikanth <rajanikanthraju.r@gmail.com>
2022-11-25 18:05:35 +0530

    Created private and public subnets and added Ec2 instances in each subnet

--- VPC/inputs.tf
+++ VPC/inputs.tf
@@ -11,78 +11,83 @@ variable "vpc_cidr" {
 }
 variable "no_of_resources" {
   type        = number
-  default     = 2
+  default     = 6
   description = "This is to set count no of required resources"
 
 }
 variable "subnet_tag_name" {
-  type        = string
-  default     = "web"
-  description = "This is the value to set the subnet name"
-
+  type        = list(string)
+  default     = ["web1", "web2", "app1", "app2", "db1", "db2"]
+  description = "This is the value to set the subnet names"
 }
-
+variable "public_subnet_tag_name" {
+  type    = list(string)
+  default = ["web1", "web2"]
+}
+variable "private_subnet_tag_name" {
+  type    = list(string)
+  default = ["app1", "app2", "db1", "db2"]
+}
 variable "all_port" {
-    type = number
-    default = 0
-    description = "all ports"
-  
+  type        = number
+  default     = 0
+  description = "all ports"
 }
 variable "all_protocol" {
-    type = number
-    default = -1
-    description = "all protocols"
-  
+  type        = number
+  default     = -1
+  description = "all protocols"
+
 }
 variable "anywhere_ip" {
-    type = string
-    default = "0.0.0.0/0"
-    description = "all ip"
-  
+  type        = string
+  default     = "0.0.0.0/0"
+  description = "all ip"
+
 }
 variable "ssh_port" {
-    type = number
-    default = 22
-    description = "ssh port"
-  
+  type        = number
+  default     = 22
+  description = "ssh port"
+
 }
 variable "tcp_protocol" {
-    type = string
-    default = "tcp"
-    description = "tcp protocol"
-  
+  type        = string
+  default     = "tcp"
+  description = "tcp protocol"
+
 }
 variable "http_port" {
-    type = number
-    default = 80
-    description = "http port"
-  
+  type        = number
+  default     = 80
+  description = "http port"
+
 }
 variable "custom_tcp_port" {
-    type = number
-    default = 8080
-    description = "8080  port"
-  
+  type        = number
+  default     = 8080
+  description = "8080  port"
+
 }
 variable "ipv6_address" {
-    type = string
-    default = "::/0"
-    description = "all ipv6"
+  type        = string
+  default     = "::/0"
+  description = "all ipv6"
 }
 
 variable "ami" {
-    type = string
-    default = "ami-0af2f764c580cc1f9"
-    description = "this is default ami"
+  type        = string
+  default     = "ami-0af2f764c580cc1f9"
+  description = "this is default ami"
 }
 variable "instance_type" {
-    type = string
-    default = "t2.micro"
-    description = "default instance type"
-  
+  type        = string
+  default     = "t2.micro"
+  description = "default instance type"
+
 }
 variable "key_path" {
-    type = string
-    default = "~/.ssh/id_rsa.pub"
-    description = "public key"  
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/eYqzVSP0X2Am1njFOXPcaY1G+rJODwciXta9l+lkv+6Mkozmr+MM3hBI97pNG/C/Ne9ISgPvsoc78gm90F/2NqNnj5LXXXme8uKtwOg3z3TaiY93707BX7D526SdS9ekRYu6sD/dP1SeHFrJUpEEEk+Hp6opOaf4Y+oV/DDRkwm7Sd7NuLcSuYcbvpaMg2Ja+QmrKhDRw396m4DJh8lJoLJJoJK2AhgQVMsU3hkxKmgk5eHbnUFjiOF4I1XeWh2IwYLwJVA9k+cQE0lQLMGpZVymFM4CuVN0D4pR1B53VOSUuuRPe3zFXqasGj5zei78Eo3WZKX6xmW6/rkW5DSb"
+  description = "public key"
 }
--- VPC/network.tf
+++ VPC/network.tf
@@ -7,62 +7,76 @@ resource "aws_vpc" "vnet" {
   }
 }
 
-#create 16 subnets each should accompany 500 hosts ipv4 addresses
-resource "aws_subnet" "subnet" {
-  count             = var.no_of_resources
+#create 2 puvlic subnets for web servers
+resource "aws_subnet" "public_subnet" {
+  count             = 2
   vpc_id            = aws_vpc.vnet.id
   cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
   availability_zone = format("${var.infra_region}%s", count.index % 2 == 0 ? "a" : "b")
   tags = {
-    "Name" = format("${var.subnet_tag_name}%d", count.index + 1)
+    "Name" = var.public_subnet_tag_name[count.index]
   }
+  depends_on = [
+    aws_vpc.vnet
+  ]
 
+}
+#create 4 private subnets for app servers & DB servers
+resource "aws_subnet" "private_subnet" {
+  count             = 4
+  vpc_id            = aws_vpc.vnet.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index+2)
+  availability_zone = format("${var.infra_region}%s", count.index % 2 == 0 ? "a" : "b")
+  tags = {
+    "Name" = var.private_subnet_tag_name[count.index]
+  }
   depends_on = [
     aws_vpc.vnet
   ]
 
 }
 
+#creation of web server security group
 resource "aws_security_group" "sg" {
-  vpc_id = aws_vpc.vnet.id
+  vpc_id      = aws_vpc.vnet.id
   description = "allow traffic as specified"
-  ingress  {
-    to_port = var.all_port
-    from_port = var.all_port
-    protocol = var.all_protocol
-    cidr_blocks = [ var.vpc_cidr]
-    description = "allow all traffic within VPC"  
-    }
-    ingress  {
-    to_port = var.http_port
-    from_port = var.http_port
-    protocol = var.tcp_protocol
+  ingress {
+    to_port     = var.all_port
+    from_port   = var.all_port
+    protocol    = var.all_protocol
+    cidr_blocks = [var.vpc_cidr]
+    description = "allow all traffic within VPC"
+  }
+  ingress {
+    to_port     = var.http_port
+    from_port   = var.http_port
+    protocol    = var.tcp_protocol
     cidr_blocks = [var.anywhere_ip]
-    description = "allow traffic on port 80 from anywhere"  
-    }
-    ingress  {
-    to_port = var.custom_tcp_port
-    from_port = var.custom_tcp_port
-    protocol = var.tcp_protocol
+    description = "allow traffic on port 80 from anywhere"
+  }
+  ingress {
+    to_port     = var.custom_tcp_port
+    from_port   = var.custom_tcp_port
+    protocol    = var.tcp_protocol
     cidr_blocks = [var.anywhere_ip]
-    description = "allow traffic on port 8080 from anywhere"  
-    }
-    ingress  {
-    to_port = var.ssh_port
-    from_port = var.ssh_port
-    protocol = var.tcp_protocol
+    description = "allow traffic on port 8080 from anywhere"
+  }
+  ingress {
+    to_port     = var.ssh_port
+    from_port   = var.ssh_port
+    protocol    = var.tcp_protocol
     cidr_blocks = [var.anywhere_ip]
-    description = "allow traffic on port 22 from anywhere"  
-    }
+    description = "allow traffic on port 22 from anywhere"
+  }
 
-    egress {
+  egress {
     from_port        = var.all_port
     to_port          = var.all_port
     protocol         = var.all_protocol
     cidr_blocks      = [var.anywhere_ip]
     ipv6_cidr_blocks = ["::/0"]
   }
-  
+
   tags = {
     "Name" = "sgfromtf"
   }
@@ -71,6 +85,36 @@ resource "aws_security_group" "sg" {
   ]
 }
 
+#creation of app server security group
+resource "aws_security_group" "asg" {
+  vpc_id      = aws_vpc.vnet.id
+  description = "allow traffic as specified"
+   ingress {
+    to_port     = var.ssh_port
+    from_port   = var.ssh_port
+    protocol    = var.tcp_protocol
+    cidr_blocks = [var.vpc_cidr]
+    description = "allow traffic on port 22 from anywhere"
+  }
+  ingress {
+    to_port     = var.all_port
+    from_port   = var.all_port
+    protocol    = var.all_protocol
+    cidr_blocks = [var.vpc_cidr]
+    description = "allow all traffic within VPC"
+  }
+
+  egress {
+    from_port        = var.all_port
+    to_port          = var.all_port
+    protocol         = var.all_protocol
+    cidr_blocks      = [var.anywhere_ip]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
+#creating Internet gateway for internet 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vnet.id
 
@@ -82,10 +126,11 @@ resource "aws_internet_gateway" "igw" {
   ]
 }
 
+#creation of public route table and attach to Internet gateway
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.vnet.id
 
-route {
+  route {
     cidr_block = var.anywhere_ip
     gateway_id = aws_internet_gateway.igw.id
   }
@@ -96,31 +141,92 @@ route {
     aws_internet_gateway.igw
   ]
 }
-resource "aws_route_table_association" "publicigw" {
-   count = var.no_of_resources
-  subnet_id = aws_subnet.subnet[count.index].id
-  route_table_id = aws_route_table.public_rt.id
-}  
+#creation of private route table 
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vnet.id
 
-resource "aws_key_pair" "key" {
-  key_name = "publickey"
-  public_key = file(var.key_path)
-  
+  tags = {
+    Name = "private_rt"
+  }
+  depends_on = [
+    aws_vpc.vnet
+  ]
 }
 
-resource "aws_instance" "ec2instance"{
-  ami = var.ami
-  instance_type = var.instance_type
+#Route table association
+resource "aws_route_table_association" "publicigw" {
+  count          = 2
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_key_pair" "key" {
+  key_name   = "publickey"
+  public_key = var.key_path
+
+}
+
+#creating Web server in public subnet
+resource "aws_instance" "ec2instance" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
   associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = aws_subnet.subnet[0].id
-  key_name = aws_key_pair.key.key_name
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.public_subnet[0].id
+  key_name                    = aws_key_pair.key.key_name
   tags = {
     "Name" = "amazonec2"
   }
-depends_on = [
-  aws_subnet.subnet,
-  aws_security_group.sg,
-  aws_key_pair.key
-]
-}  
+  provisioner "file" {
+  source      = "~/Downloads/awsclipractice.pem"
+  destination = "awsclipractice.pem"
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("~/Downloads/awsclipractice.pem")
+    host     = self.public_ip
+  }
+}
+connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("~/Downloads/awsclipractice.pem")
+    host     = self.public_ip
+  }
+provisioner "remote-exec" {
+    inline = [
+      "sudo chmod 400 awsclipractice.pem"
+    ]
+  }
+  depends_on = [
+    aws_subnet.public_subnet,
+    aws_security_group.sg,
+    aws_key_pair.key
+  ]
+}
+#creating app server in private  subnet 
+resource "aws_instance" "privateec2instance" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.asg.id]
+  subnet_id                   = aws_subnet.private_subnet[0].id
+  key_name                    = aws_key_pair.key.key_name
+  tags = {
+    "Name" = "privateamazonec2"
+  }
+  depends_on = [
+    aws_subnet.private_subnet,
+    aws_security_group.asg,
+    aws_key_pair.key
+  ]
+}
+
+
+output "publicserverip" {
+  value = aws_instance.ec2instance.public_ip
+  
+}
+output "privateserverip" {
+  value= aws_instance.privateec2instance.private_ip
+}
